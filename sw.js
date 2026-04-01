96b2bf19252ea3c63279096777627d80833ed92e
harshulmoon <152827867+harshulmoon@users.noreply.github.com>
2026-04-01 20:33:57 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-const CACHE_NAME = 'compass-v1';
-
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((cached) => {
-            return cached || fetch(e.request).then((response) => {
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        }).catch(() => caches.match('/'))
-    );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
